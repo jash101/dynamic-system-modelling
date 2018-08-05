c33e91af2b0d3ad94346304d75f778539b23c0d9
Jash Mota <jashmota101@gmail.com>
2018-08-05 13:12:41 +0530

    Create jaco.m

---
+++ jaco.m
@@ -0,0 +1,13 @@
+%Calculate matrix A and B using Jacobians
+
+%Check matrices A and B in command window by typing 'a' or 'b'
+syms x y u v
+
+eq1 = x^2 + 2*x*y + y^2 + 2*u;
+
+eq2 = x*y + y^2 + 4*x + 4*v;
+
+%jacobians helps in local linearization of non linear systems
+a = jacobian([eq1, eq2], [x, y]);
+
+b = jacobian([eq1, eq2], [u, v]);
