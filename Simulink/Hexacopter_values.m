e80138703790bc178c35ade8484f4cfd81e53266
Jash Mota <jashmota101@gmail.com>
2018-08-29 17:40:39 +0530

    added simulink files

---
+++ Simulink/Hexacopter_values.m
@@ -0,0 +1,22 @@
+Jxx=7.5*10^-3
+Jyy=7.5*10^-3
+Jzz=1.3*10^-2
+l=0.23
+Jr=6*exp(-5)
+m=0.65
+b=3.13*exp(-5)
+Km=7.5*exp(-7)
+Rmot=0.6
+Kmot=5.2
+v=[0.1 0.1 0.1]
+Kt=diag(v)
+Kftx=1.5
+Kfty=0.1
+Kftz=1.9
+Kfax=0.1
+
+Kfay=0.1
+Kfaz=0.15
+Ki=1
+Kd=0.1
+Kp=1000
