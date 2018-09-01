2764a84e9be96f8d550383cd1d36787397f71c34
Jash Mota <jashmota101@gmail.com>
2018-09-01 17:04:32 +0530

    stable version

--- hexacontrol.m
+++ hexacontrol.m
@@ -4,14 +4,14 @@ u = zeros(4,1);
 e = zeros(8,1);%Steady state error matrix
 
 z = 10; %Reference height
-kp = 0.05; ki = 0.1; kd = 0.1;%error coeffs
-e(1) = z-Y(9);
+kp = 10000; ki = 20; kd = 40;%error coeffs
+e(1) = z-Y(11);
 u(1) = kp*e(1) + ki*(e(1) + e(2)) + kd*(e(1) - e(2));
 e(2) = e(1);
 
 %phi reference
 phi = 0;
-kp = 0.05; ki = 0.1; kd = 0.1;%error coeffs
+kp = 0.05; ki = 0.1; kd = 1;%error coeffs
 e(3) = phi-Y(1);
 u(2) = kp*e(3) + ki*(e(3) + e(4)) + kd*(e(3) - e(4));
 e(4) = e(3);
