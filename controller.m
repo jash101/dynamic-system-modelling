6f373c613fd4d68ef02edd6fa5962b0c95d87db6
Jash Mota <jashmota101@gmail.com>
2018-09-02 13:23:54 +0530

    conflict cleared

--- controller.m
+++ controller.m
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-function V = controller (t,Y)
-e = zeros(3,1);%Steady state error matrix
-w = 1000; %Reference speed
-
-kp = 0.05; ki = 0.1; kd = 0.1;%error coeffs
-
-e(1) = w-Y(2);
-
-e(3) = e(3) + e(1);
-
-V = kp*e(1) + ki*(e(3)) + kd*(e(1) - e(2));
-
-e(2) = e(1);
-
-% plot(t,e(1));
-
-end
-=======
 function V = controller(t,y)
 global Esum;
 global Eprev;
@@ -36,4 +17,3 @@ V = Kp*e + Ki*Esum + Kd*(e - Eprev);
 Eprev = e;
 
 end
->>>>>>> efca026f8d901dbfd2d87ac38cd9bf7ea428c121
