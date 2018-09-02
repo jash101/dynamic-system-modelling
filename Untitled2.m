a0d55f6a78c77807c50c7c97bd28e50d6a683cab
Jash Mota <jashmota101@gmail.com>
2018-09-02 20:02:44 +0530

    works perfectly
    integration logic tweaked

--- Untitled2.m
+++ Untitled2.m
@@ -1,10 +1,14 @@
-Y = zeros(12, 1);
-t = 0:0.01:2000; 
-% options = odeset('OutputFcn', @odeplot);
-[Mt, My] = ode45(@hexa, t, Y);%solve ode and get mt, my
+clear;
+close all;
+clc;
 
-%plot mt, my
-figure(1);
-plot (Mt, My(:,1) );
-figure(2);
+Y = zeros(12, 1);
+t = 0:1:100; 
+options = odeset('OutputFcn', @odeplot);
+[Mt, My] = ode23(@hexa, t, Y);%solve ode and get mt, my
+
+%%plot mt, my
+% figure(1);
+% plot (Mt, My(:,1) );
+% figure(2);
 plot (Mt, My(:,11) );
