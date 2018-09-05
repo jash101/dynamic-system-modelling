80a03fe381b172addb1d80eaac84a6383b01590f
Jash Mota <jashmota101@gmail.com>
2018-09-05 18:29:54 +0530

    display matrices

--- jaco.m
+++ jaco.m
@@ -8,6 +8,6 @@ eq1 = x^2 + 2*x*y + y^2 + 2*u;
 eq2 = x*y + y^2 + 4*x + 4*v;
 
 %jacobians helps in local linearization of non linear systems
-a = jacobian([eq1, eq2], [x, y]);
+a = jacobian([eq1, eq2], [x, y])
 
-b = jacobian([eq1, eq2], [u, v]);
+b = jacobian([eq1, eq2], [u, v])
