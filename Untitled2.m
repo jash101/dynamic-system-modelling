b22f25345345e549e9cc8394012070076b661e1c
Jash Mota <jashmota101@gmail.com>
2018-09-05 08:20:22 +0530

    Less samples, ode23 used

--- Untitled2.m
+++ Untitled2.m
@@ -1,11 +1,19 @@
 clear;
-close all;
+%close all;
 clc;
+global e;
+global e_curr;
+global e_prev;
+global e_sum;
+e_sum = 0;
+e_prev = 0;
+e_curr = 0;
 
+%e = zeros(1,8);%Steady state error matrix
 Y = zeros(12, 1);
-t = 0:1:100; 
+t = 0:1:25; 
 options = odeset('OutputFcn', @odeplot);
-[Mt, My] = ode23(@hexa, t, Y);%solve ode and get mt, my
+[Mt, My] = ode45(@hexa, t, Y);%solve ode and get mt, my
 
 %%plot mt, my
 % figure(1);
